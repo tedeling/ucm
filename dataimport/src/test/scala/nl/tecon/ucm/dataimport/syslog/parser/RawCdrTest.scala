419a6b2ef451946af3136699395cd1b1c6c7cc86
Thies Edeling <thies@te-con.nl>
2012-06-24 23:48:12 +0200

    spec fix

--- dataimport/src/test/scala/nl/tecon/ucm/dataimport/syslog/parser/RawCdrTest.scala
+++ dataimport/src/test/scala/nl/tecon/ucm/dataimport/syslog/parser/RawCdrTest.scala
@@ -6,7 +6,7 @@ class RawCdrTest extends SpecificationWithJUnit {
   "The RawCDR" should {
     val cdr = RawCdr(" CME-MRI: Dec 16 10:16:25.486: %VOIPAAA-5-VOIP_CALL_HISTORY: CallLegType 2, ConnectionId CA5210B082B11E0A142A279BD84410, SetupTime 10:14:59.214 CET Thu Dec 16 2010, PeerAddress 510, PeerSubAddress , DisconnectCause 10  , DisconnectText normal call clearing (16), ConnectTime 10:14:59.294 CET Thu Dec 16 2010, DisconnectTime 10:15:50.894 CET Thu Dec 16 2010, CallOrigin 1, ChargedUnits 0, InfoType 2, TransmitPackets 2562, TransmitBytes 409920, ReceivePackets 2576, ReceiveBytes 411524")
     "get a type" in {
-      cdr.cdrType() must equalTo("%VOIPAAA-5-VOIP_CALL_HISTORY")
+      cdr.cdrType() must beSome("%VOIPAAA-5-VOIP_CALL_HISTORY")
     }
   }
 
